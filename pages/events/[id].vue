99b861e9a852e8b70c88eff50a3110f6ef7ece9c
Leon Gaban <leongaban@gmail.com>
2024-08-16 21:15:17 -0500

    added starter pages

---
+++ pages/events/[id].vue
@@ -0,0 +1,9 @@
+<script setup>
+const nuxt = useNuxtApp();
+const route = useRoute();
+console.log(nuxt);
+</script>
+
+<template>
+  <h1>Event id: {{  $route.params.id }}</h1>
+</template>
---
+++ pages/events/profile.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Event Profile</h1>
+</template>
